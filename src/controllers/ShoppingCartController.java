52102deee2b3c2dc6ff502055c96e7bc0e8dbb45
Rstrandell <razmus@student.chalmers.se>
2016-02-21 22:00:58 +0100

    fixade så man kan komma tillbaka till butiken från kundvagnen

--- src/controllers/ShoppingCartController.java
+++ src/controllers/ShoppingCartController.java
@@ -1,13 +1,20 @@
 package controllers;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 
 
 /**
@@ -67,6 +74,14 @@ public class ShoppingCartController {
         incItem.setVisible(false);
         decItem.setVisible(false);
         price.setVisible(false);
+    }
+    public void backToStore(ActionEvent event)throws IOException {
+        Parent storeParent = FXMLLoader.load(getClass().getResource("/fxml/imat.fxml"));
+        Scene storeScene = new Scene(storeParent);
+        Stage storeStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        storeStage.hide();
+        storeStage.setScene(storeScene);
+        storeStage.show();
     }
 
 
