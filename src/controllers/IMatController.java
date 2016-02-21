9b8a45b792d3a1fd21f88a2760e150bb63afbc42
Rstrandell <razmus@student.chalmers.se>
2016-02-21 21:57:05 +0100

    har gjort så man kan gå till kundvagnen typ, funkar inte via menu item utan det krävs en knapp, tar även bort själva menyn runt omkring men är en början!

--- src/controllers/IMatController.java
+++ src/controllers/IMatController.java
@@ -2,13 +2,20 @@ package controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import se.chalmers.ait.dat215.project.ProductCategory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
@@ -50,4 +57,14 @@ public class IMatController implements Initializable {
         if (p.toString().equalsIgnoreCase("HERB")) return "Örter och kryddor";
         return null;
     }
+    public void goToCart(ActionEvent event)throws IOException {
+        Parent cartParent = FXMLLoader.load(getClass().getResource("/fxml/shoppingcart.fxml"));
+        Scene cartScene = new Scene(cartParent);
+        Stage cartStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        cartStage.hide();
+        cartStage.setScene(cartScene);
+        cartStage.show();
+    }
+
+
 }
