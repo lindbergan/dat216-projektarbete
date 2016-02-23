f8b4830d59c8abe96b883c636dc9a44f121e8aec
Rstrandell <razmus@student.chalmers.se>
2016-02-23 20:41:35 +0100

    fixade tillbaka till butik knappens funktion

--- src/controllers/ShoppingCartController.java
+++ src/controllers/ShoppingCartController.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -20,54 +21,68 @@ import java.io.IOException;
 /**
  * Created by Razmus on 2016-02-21.
  */
-public class ShoppingCartController {
-    @FXML private GridPane cartPane;
-    @FXML private Pane cartView;
-    @FXML private TextField cartAmount;
-    @FXML private Label itemName;
-    @FXML private Button decItem;
-    @FXML private Button incItem;
-    @FXML private Label price;
+public class ShoppingCartController extends IMatController {
+    @FXML
+    private GridPane cartPane;
+    @FXML
+    private Pane cartView;
+    @FXML
+    private TextField cartAmount;
+    @FXML
+    private Label itemName;
+    @FXML
+    private Button decItem;
+    @FXML
+    private Button incItem;
+    @FXML
+    private Button homeButton;
+    @FXML
+    private Label price;
+    @FXML
+    private AnchorPane nobPane;
 
 
-    public void preventNull(){
-        if (!cartAmount.isFocused() && cartAmount.getText() == null){
+
+    public void preventNull() {
+        if (!cartAmount.isFocused() && cartAmount.getText() == null) {
             cartAmount.setText("1");
         }
     }
 
-    public void cartAmountClicked(){
+    public void cartAmountClicked() {
         cartAmount.setText(null);
 
 
     }
-    public void incItem(){
-        if(cartAmount.getText() != null) {
+
+    public void incItem() {
+        if (cartAmount.getText() != null) {
             int old = Integer.parseInt(cartAmount.getText());
             int tmp = old + 1;
             String updated = "" + tmp;
             cartAmount.setText(updated);
-        }
-        else cartAmount.setText("1");
+        } else cartAmount.setText("1");
     }
-    public void decItem(){
-        if(cartAmount.getText() != null) {
+
+    public void decItem() {
+        if (cartAmount.getText() != null) {
             int old = Integer.parseInt(cartAmount.getText());
             if (old != 0 && old != 1) {
                 int tmp = old - 1;
                 String updated = "" + tmp;
                 cartAmount.setText(updated);
             }
-        }
-        else cartAmount.setText("1");
+        } else cartAmount.setText("1");
     }
-    public void preventZero(){
+
+    public void preventZero() {
         int old = Integer.parseInt(cartAmount.getText());
-        if(old == 0){
+        if (old == 0) {
             cartAmount.setText("1");
         }
     }
-    public void deleteItem(){
+
+    public void deleteItem() {
         preventNull();
         itemName.setVisible(false);
         cartAmount.setVisible(false);
@@ -75,14 +90,18 @@ public class ShoppingCartController {
         decItem.setVisible(false);
         price.setVisible(false);
     }
-    public void backToStore(ActionEvent event)throws IOException {
-        Parent storeParent = FXMLLoader.load(getClass().getResource("/fxml/imat.fxml"));
-        Scene storeScene = new Scene(storeParent);
-        Stage storeStage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        storeStage.hide();
-        storeStage.setScene(storeScene);
-        storeStage.show();
+
+    public void setPane()throws IOException{
+        AnchorPane e = FXMLLoader.load(getClass().getResource("/fxml/categoryMenu.fxml/"));
+        nobPane.getChildren().setAll(e);
+
     }
 
 
+
 }
+
+
+
+
+
