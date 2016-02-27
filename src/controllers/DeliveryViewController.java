1f1791b3ea5d5bb32f67f44c2a6723c210e0c6ea
Jolo87 <nylundj@student.chalmers.se>
2016-02-27 15:55:03 +0100

    Listeners till textfälten sparande till Customer.

--- src/controllers/DeliveryViewController.java
+++ src/controllers/DeliveryViewController.java
@@ -21,33 +21,54 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import se.chalmers.ait.dat215.project.Customer;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
 //skriver slutligen en vy och en klass för den sista "rutan" med tack för ditt köp, fortsätt handla osv..
 public class DeliveryViewController implements Initializable{
 
-    @FXML private MenuBar menuBar;
     @FXML private AnchorPane deliveryView;
-    @FXML private AnchorPane paymentViewCard;
-    @FXML private AnchorPane paymentViewInvoice;
-    @FXML private AnchorPane confimationView;
     @FXML private ImageView iMatLogo; //photoshop, labeled buttons
-
-
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private Customer customer = handler.getCustomer();
 
     final ToggleGroup radioButtonGroup = new ToggleGroup();
     @FXML private RadioButton cardPayment;
     @FXML private RadioButton invoicePayment;
 
+    @FXML private TextField customerFirstName;
+    @FXML private TextField customerLastName;
+    @FXML private TextField customerAddress;
+    @FXML private TextField customerPostCode;
+    @FXML private TextField customerPostAddress;
+    @FXML private TextField customerEmail;
+    @FXML private TextField customerPhone;
+
+
 
     @Override
-
-    //sets the radiobuttons, card payment = default choise
     public void initialize(URL url, ResourceBundle rb) {
 
+        //sets the textfields to the correct default values
+        customerFirstName.setText(customer.getFirstName());
+        customerLastName.setText(customer.getLastName());
+        customerAddress.setText(customer.getAddress());
+        customerPostCode.setText(customer.getPostCode());
+        customerPostAddress.setText(customer.getPostAddress());
+        customerEmail.setText(customer.getEmail());
+        customerPhone.setText(customer.getPhoneNumber());
+
+        //sets the radiobuttons, card payment = default choise
         cardPayment.setSelected(true);
         invoicePayment.setSelected(false);
         cardPayment.setToggleGroup(radioButtonGroup);
         invoicePayment.setToggleGroup(radioButtonGroup);
+
+        this.listenToTextField();
     }
+
+
+
 
     //back to IMat store when user clicked the Logotype
     public void logoClicked(ActionEvent event) throws IOException{
@@ -72,7 +93,7 @@ public class DeliveryViewController implements Initializable{
         mainStage.show();
     }
 
-    //gives us the right payment view
+    //gives us the right payment view depending on what radiobutton is selected
     public void continueClicked()throws IOException{
 
         if(cardPayment.isSelected()){
@@ -85,4 +106,65 @@ public class DeliveryViewController implements Initializable{
         }
     }
 
+
+    public void listenToTextField(){
+
+        //ChangeListener for the textfield:
+
+        //firstName:
+        customerFirstName.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setFirstName(newValue);
+            }
+        });
+
+        //LastName:
+        customerLastName.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setLastName(newValue);
+            }
+        });
+
+        //Adress:
+        customerAddress.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setAddress(newValue);
+            }
+        });
+
+        //PostCode:
+        customerPostCode.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setPostCode(newValue);
+            }
+        });
+
+        //PostAdress:
+        customerPostAddress.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setPostAddress(newValue);
+            }
+        });
+
+        //Email:
+        customerEmail.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setEmail(newValue);
+            }
+        });
+
+        //PhoneNumber:
+        customerPhone.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                customer.setPhoneNumber(newValue);
+            }
+        });
+    }
 }
