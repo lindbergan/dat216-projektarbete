22397703cf5c04375d468e938673e6c43cdb879e
Jolo87 <nylundj@student.chalmers.se>
2016-02-27 16:23:32 +0100

    InvoiceController: ändrat så att "samma som leveransadress" är förvalt och "annan adress" är disabled (finns ingen hantering för faktura i backend vad jag kan se). Satte därmed alla textfält till disabled så att de hämtar samma som för leveransuppg. därmed är Invoice = i princip klar.

--- src/controllers/InvoiceController.java
+++ src/controllers/InvoiceController.java
@@ -11,13 +11,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import se.chalmers.ait.dat215.project.Customer;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -29,16 +29,36 @@ public class InvoiceController implements Initializable {
     final ToggleGroup radioButtonGroup = new ToggleGroup();
     @FXML private RadioButton sameAsDelivery;
     @FXML private RadioButton otherAdress;
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private Customer customer = handler.getCustomer();
+
+
+    @FXML private TextField invoiceFirstName;
+    @FXML private TextField invoiceLastName;
+    @FXML private TextField invoiceAddress;
+    @FXML private TextField invoicePostCode;
+    @FXML private TextField invoicePostAddress;
+    @FXML private TextField invoiceEmail;
+    @FXML private TextField invoicePhone;
 
 
 
     @Override
-    //sets the radiobuttons, visa = default choise
     public void initialize(URL url, ResourceBundle rb) {
 
+        //sets the radiobuttons, visa = default choise, user specified option is set to diabled due to time restraints
         sameAsDelivery.setSelected(true);
         sameAsDelivery.setToggleGroup(radioButtonGroup);
         otherAdress.setToggleGroup(radioButtonGroup);
+
+        //sets the textfields to the same as delivery by default
+        invoiceFirstName.setText(customer.getFirstName());
+        invoiceLastName.setText(customer.getLastName());
+        invoiceAddress.setText(customer.getAddress());
+        invoicePostCode.setText(customer.getPostCode());
+        invoicePostAddress.setText(customer.getPostAddress());
+        invoiceEmail.setText(customer.getEmail());
+        invoicePhone.setText(customer.getPhoneNumber());
     }
 
 
