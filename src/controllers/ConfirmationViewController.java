4e00a69c2658faf729b4f323e543395c124ae9e0
Jolo87 <nylundj@student.chalmers.se>
2016-02-27 20:10:14 +0100

    tror denna klass är överflödig om jag tänkt rätt, gör en backup för säkerhets skull

--- src/controllers/ConfirmationViewController.java
+++ src/controllers/ConfirmationViewController.java
@@ -11,13 +11,13 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import se.chalmers.ait.dat215.project.Customer;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -25,15 +25,36 @@ import java.util.ResourceBundle;
 public class ConfirmationViewController implements Initializable {
 
     @FXML private AnchorPane confirmationView;
+    @FXML private AnchorPane exitView;
+    @FXML private Label customerName;
+    @FXML private Label customerAddress;
+    @FXML private Label customerPostCode;
+    @FXML private Label customerEmail;
+    @FXML private Label customerPhone;
+    @FXML private Label customerDate;
+
+
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private Customer customer = handler.getCustomer();
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        customerName.setText(customer.getFirstName() +customer.getLastName());
+        customerAddress.setText(customer.getAddress());
+        customerPostCode.setText(customer.getPostCode());
+        customerEmail.setText(customer.getEmail());
+        customerPhone.setText(customer.getPhoneNumber());
+        customerDate.setText(DeliveryViewController.getUserSpecifiedDate() + " " +
+                DeliveryViewController.getUserSpecifiedMonth() + " mellan kl " +
+                DeliveryViewController.getUserSpecifiedMinTime() + " - " +
+                DeliveryViewController.getUserSpecifiedMaxTime()  );
     }
 
     //back to payment view when clicked <--
     public void backtoPaymentView()throws IOException {
 
-        //villkor - beroende på om kund valt kort eller faktura,
+        //villkor - beroende på om kund valt kort eller faktura, löses med observables och firepropertychange??
 
         // kort blir:
         AnchorPane paymentViewCard = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewCard.fxml/"));
