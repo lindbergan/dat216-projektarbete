3bd06b951a88d5efc36bcfb05e5d4e37e5e0de1e
Jolo87 <nylundj@student.chalmers.se>
2016-02-28 15:58:20 +0100

    CreditCardController, senaste versionen 02-28

--- src/controllers/CreditCardController.java
+++ src/controllers/CreditCardController.java
@@ -4,6 +4,8 @@ package controllers;
  * Created by Jolo on 2/26/16.
  */
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -11,16 +13,17 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import se.chalmers.ait.dat215.project.CreditCard;
+import se.chalmers.ait.dat215.project.Customer;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
 
 public class CreditCardController implements Initializable {
 
@@ -33,12 +36,18 @@ public class CreditCardController implements Initializable {
     @FXML private RadioButton visa;
     @FXML private RadioButton mastercard;
     @FXML private RadioButton other;
-
+    @FXML private TextField cardHolderName;
+    private static boolean visited = false;
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private Customer customer = handler.getCustomer();
 
 
     @Override
     //sets the radiobuttons, visa = default choise
     public void initialize(URL url, ResourceBundle rb) {
+
+        //makes sure that it has been visited in order to be able to jump pass it from the buttons in the header
+        visited = true;
 
         visa.setSelected(true);
         visa.setToggleGroup(radioButtonGroup);
@@ -49,19 +58,19 @@ public class CreditCardController implements Initializable {
     //back to deliveryView when clicked <--
     public void backToDeliveryClicked(ActionEvent event)throws IOException {
 
-        Parent checkoutParent = FXMLLoader.load(getClass().getResource("/fxml/CheckoutView.fxml/"));
-        Scene checkoutScene = new Scene(checkoutParent);
-        Stage checkoutStage = (Stage)((Node) event.getSource()).getScene().getWindow();
-        checkoutStage.hide();
-        checkoutStage.setScene(checkoutScene);
-        checkoutStage.show();
+        AnchorPane deliveryView = FXMLLoader.load(getClass().getResource("/fxml/DeliveryView.fxml"));
+        paymentViewCard.getChildren().setAll(deliveryView);
+
     }
 
     //gives us the confirmation view
     public void continueClicked()throws IOException{
 
-        AnchorPane confirmationView = FXMLLoader.load(getClass().getResource("/fxml/ConfirmationView.fxml/"));
+        AnchorPane confirmationView = FXMLLoader.load(getClass().getResource("/fxml/ConfirmationView.fxml"));
         paymentViewCard.getChildren().setAll(confirmationView);
+    }
 
+    public static boolean hasBeenVisited(){
+        return visited;
     }
 }
