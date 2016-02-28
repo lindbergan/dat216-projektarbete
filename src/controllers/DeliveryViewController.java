53fb33baaff4fe6a0c17b9fd7103c34d8eb4441f
Jolo87 <nylundj@student.chalmers.se>
2016-02-28 15:57:38 +0100

    DeliveryViewController, senaste versionen 02-28

--- src/controllers/DeliveryViewController.java
+++ src/controllers/DeliveryViewController.java
@@ -1,8 +1,9 @@
 package controllers;
 
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,7 +17,6 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -24,11 +24,10 @@ import java.util.ResourceBundle;
 import se.chalmers.ait.dat215.project.Customer;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
 
-//skriver slutligen en vy och en klass för den sista "rutan" med tack för ditt köp, fortsätt handla osv..
 public class DeliveryViewController implements Initializable{
 
     @FXML private AnchorPane deliveryView;
-    @FXML private ImageView iMatLogo; //photoshop, labeled buttons
+    @FXML private ImageView iMatLogo;
     IMatDataHandler handler = IMatDataHandler.getInstance();
     private Customer customer = handler.getCustomer();
 
@@ -44,7 +43,22 @@ public class DeliveryViewController implements Initializable{
     @FXML private TextField customerEmail;
     @FXML private TextField customerPhone;
 
+    @FXML private ChoiceBox monthChoisebox;
+    @FXML private ChoiceBox dateChoisebox;
+    @FXML private ChoiceBox minTimeChoisebox;
+    @FXML private ChoiceBox maxTimeChoisebox;
 
+    //the observable lists for the Choiseboxes
+    private ObservableList<String> month = FXCollections.observableArrayList("Januari","Februari", "Mars","April","Maj","Juni","Juli","Augusti","September","Oktober","November","December");
+    private ObservableList<String> date = FXCollections.observableArrayList("1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31");
+    private ObservableList<String> minTime = FXCollections.observableArrayList("00","01", "02","03","04","05","06","07","08","09","10","11", "12","13","14","15","16","17","18","19","20","21","22","23","24");
+    private ObservableList<String> maxTime = FXCollections.observableArrayList("00","01", "02","03","04","05","06","07","08","09","10","11", "12","13","14","15","16","17","18","19","20","21","22","23","24");
+
+    private static String userSpecifiedMonth;
+    private static String userSpecifiedDate;
+    private static String userSpecifiedMinTime;
+    private static String userSpecifiedMaxTime;
+    private static String paymentChoise = "Kortbetalning"; //default set to card
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -58,31 +72,56 @@ public class DeliveryViewController implements Initializable{
         customerEmail.setText(customer.getEmail());
         customerPhone.setText(customer.getPhoneNumber());
 
-        //sets the radiobuttons, card payment = default choise
-        cardPayment.setSelected(true);
-        invoicePayment.setSelected(false);
+        //initialize the choiseboxes
+        monthChoisebox.setItems(month);
+        dateChoisebox.setItems(date);
+        minTimeChoisebox.setItems(minTime);
+        maxTimeChoisebox.setItems(maxTime);
+
+
+        //sets the radiobuttons
+        if(paymentChoise == "Kortbetalning") {
+            cardPayment.setSelected(true);
+            invoicePayment.setSelected(false);
+        }
+        else{
+            cardPayment.setSelected(false);
+            invoicePayment.setSelected(true);
+        }
         cardPayment.setToggleGroup(radioButtonGroup);
         invoicePayment.setToggleGroup(radioButtonGroup);
 
+        //to detemine which radiobutton was chosen
+        radioButtonGroup.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+            @Override
+            public void changed(ObservableValue<? extends Toggle> observable, Toggle oldValue, Toggle newValue) {
+
+                if(newValue == cardPayment){
+                    paymentChoise = "Kortbetalning";
+                }
+                else{
+                    paymentChoise = "Fakturabetalning";
+                }
+            }
+        });
+
+        //listen to all the fields user can change
         this.listenToTextField();
+        this.listenToChoiseboxes();
     }
-
-
-
 
     //back to IMat store when user clicked the Logotype
     public void logoClicked(ActionEvent event) throws IOException{
 
-            Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/IMat.fxml"));
-            Scene mainScene = new Scene(mainParent);
-            Stage mainStage = (Stage)((Node) event.getSource()).getScene().getWindow();
-            mainStage.hide();
-            mainStage.setScene(mainScene);
-            mainStage.show();
-
+        Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/IMat.fxml"));
+        Scene mainScene = new Scene(mainParent);
+        Stage mainStage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        mainStage.hide();
+        mainStage.setScene(mainScene);
+        mainStage.show();
     }
 
-    //back to IMat store when user clicked <--
+    //back to IMat store when user clicked "back" <--
     public void backtoStoreClicked(ActionEvent event) throws IOException {
 
         Parent mainParent = FXMLLoader.load(getClass().getResource("/fxml/IMat.fxml"));
@@ -96,20 +135,55 @@ public class DeliveryViewController implements Initializable{
     //gives us the right payment view depending on what radiobutton is selected
     public void continueClicked()throws IOException{
 
-        if(cardPayment.isSelected()){
-            AnchorPane cardView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewCard.fxml/"));
+        if(paymentChoise == "Kortbetalning"){
+            AnchorPane cardView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewCard.fxml"));
             deliveryView.getChildren().setAll(cardView);
         }
         else {
-            AnchorPane invoiceView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewInvoice.fxml/"));
+            AnchorPane invoiceView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewInvoice.fxml"));
             deliveryView.getChildren().setAll(invoiceView);
         }
     }
 
+    //ChangeListener for the choiseboxes
+    public void listenToChoiseboxes(){
 
+        //selected month
+        monthChoisebox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int choiseBoxIndex = newValue.intValue();
+                userSpecifiedMonth = month.get(choiseBoxIndex);
+            }
+        });
+        //selected date
+        dateChoisebox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int choiseBoxIndex = newValue.intValue();
+                userSpecifiedDate = date.get(choiseBoxIndex);
+            }
+        });
+        //selected minTime
+        minTimeChoisebox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int choiseBoxIndex = newValue.intValue();
+                userSpecifiedMinTime = minTime.get(choiseBoxIndex);
+            }
+        });
+        //selected maxTime
+        maxTimeChoisebox.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                int choiseBoxIndex = newValue.intValue();
+                userSpecifiedMaxTime = maxTime.get(choiseBoxIndex);
+            }
+        });
+    }
+
+    //ChangeListener for the textfields:
     public void listenToTextField(){
-
-        //ChangeListener for the textfield:
 
         //firstName:
         customerFirstName.textProperty().addListener(new ChangeListener<String>() {
@@ -167,4 +241,47 @@ public class DeliveryViewController implements Initializable{
             }
         });
     }
+
+    //Associate the different buttons in the header to the corresponding View
+    public void DeliveryButtonPushed()throws IOException{
+            AnchorPane delView = FXMLLoader.load(getClass().getResource("/fxml/DeliveryView.fxml"));
+            deliveryView.getChildren().setAll(delView);
+    }
+    public void PaymentButtonPushed()throws IOException{
+
+        if(paymentChoise == "Kortbetalning"){
+            AnchorPane cardView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewCard.fxml"));
+            deliveryView.getChildren().setAll(cardView);
+        }
+        else {
+            AnchorPane invoiceView = FXMLLoader.load(getClass().getResource("/fxml/PaymentViewInvoice.fxml"));
+            deliveryView.getChildren().setAll(invoiceView);
+        }
+    }
+    public void ConfirmationButtonPushed()throws IOException{
+
+        //makes sure that the customer has been to InvoiceView or CardView before entering ConfirmationView.
+        if(InvoiceController.hasBeenVisited() || CreditCardController.hasBeenVisited()) {
+            AnchorPane confirmationView = FXMLLoader.load(getClass().getResource("/fxml/ConfirmationView.fxml"));
+            deliveryView.getChildren().setAll(confirmationView);
+        }
+    }
+
+    //The getters for our custom choisboxes and radiobuttons:
+    public static String getUserSpecifiedMonth(){
+        return userSpecifiedMonth;
+    }
+    public static String getUserSpecifiedDate(){
+        return userSpecifiedDate;
+    }
+    public static String getUserSpecifiedMinTime(){
+        return userSpecifiedMinTime;
+    }
+    public static String getUserSpecifiedMaxTime(){
+        return userSpecifiedMaxTime;
+    }
+    public static String getPaymentChoise(){
+        return paymentChoise;
+    }
+
 }
