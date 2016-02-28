d6e3420549a2ba17a1b7155f69bfc97a215e5035
Jolo87 <nylundj@student.chalmers.se>
2016-02-28 15:57:02 +0100

    ExitViewController, senaste versionen 02-28

--- src/controllers/ExitViewController.java
+++ src/controllers/ExitViewController.java
@@ -12,30 +12,49 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import se.chalmers.ait.dat215.project.Customer;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ExitViewController implements Initializable {
 
-    @FXML private AnchorPane exitView;
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private Customer customer = handler.getCustomer();
+    @FXML private Label customerEmail;
+    @FXML private Label customerDate;
+
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+
+        customerEmail.setText(customer.getEmail());
+        customerDate.setText(DeliveryViewController.getUserSpecifiedDate() + " " +
+                DeliveryViewController.getUserSpecifiedMonth() + " mellan kl " +
+                DeliveryViewController.getUserSpecifiedMinTime() + " - " +
+                DeliveryViewController.getUserSpecifiedMaxTime());
     }
 
     //goes back to the store
     public void continueShopping(ActionEvent event) throws IOException{
-        Parent imatParent = FXMLLoader.load(getClass().getResource("/fxml/IMat.fxml/"));
+
+        Parent imatParent = FXMLLoader.load(getClass().getResource("/fxml/IMat.fxml"));
         Scene imatScene = new Scene(imatParent);
         Stage imatStage = (Stage)((Node) event.getSource()).getScene().getWindow();
         imatStage.hide();
         imatStage.setScene(imatScene);
         imatStage.show();
     }
+
+    //closes the application
     public void closeWindow(ActionEvent event){
         ((Node)(event.getSource())).getScene().getWindow().hide();
     }
