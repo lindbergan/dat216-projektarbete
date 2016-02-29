59b9eff807747ce4bf3badbd2a14ddce83014d5a
Adrian Lindberg <adrlin@student.chalmers.se>
2016-02-29 19:32:57 +0100

    Nu lägger kategorivyn till en massa buttons. En button för varje produkt som hämtas. Ska fixa så att den hämtar en för varje produkt i en speciellproduktkategori

--- src/controllers/categoryMenuController.java
+++ src/controllers/categoryMenuController.java
@@ -2,6 +2,10 @@ package controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import se.chalmers.ait.dat215.project.IMatDataHandler;
 import se.chalmers.ait.dat215.project.Product;
@@ -14,15 +18,25 @@ import java.util.ResourceBundle;
 public class categoryMenuController implements Initializable {
 
     @FXML public GridPane gridPane;
+    @FXML public AnchorPane apGridWindow;
     IMatDataHandler handler = IMatDataHandler.getInstance();
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        showProducts();
     }
+
+    /*public void getSelectedCategory() {
+        showProductsPC();
+        showProducts();
+    }*/
 
     public void showProducts() {
         List<Product> productList = handler.getProducts();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.TOP_CENTER);
+        gridPane.setPadding(new Insets(10));
         if (!(productList.isEmpty())) {
             int productListSize = productList.size();
             int rowNr = 0;
@@ -30,7 +44,23 @@ public class categoryMenuController implements Initializable {
                 gridPane.addRow(rowNr);
                 rowNr++;
             }
-            rowNr = 0;
+            int rowNrAgain = 0;
+            double magicalHeight = 483.0;
+            int magicalIdNr = 0;
+            for (int i = 0; i < productListSize - 1; i+=4) {
+                for (int j = 0; j < 4; j++) {
+                    Button newButton = new Button("" + magicalIdNr);
+                    newButton.setPrefWidth(200);
+                    newButton.setPrefHeight(250);
+                    gridPane.add(newButton, j, rowNrAgain);
+                    magicalIdNr++;
+                }
+                if (rowNrAgain < rowNr) {
+                    rowNrAgain++;
+                    apGridWindow.setPrefHeight(magicalHeight + 250);
+                    magicalHeight = magicalHeight + 250;
+                }
+            }
         }
         else {
             System.out.println("Productlist is empty. ");
@@ -38,7 +68,40 @@ public class categoryMenuController implements Initializable {
 
     }
 
-    public void showProducts(ProductCategory productCategory) {
+    public void showProductsPC() {
+        List<Product> productList = handler.getProducts();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setAlignment(Pos.TOP_CENTER);
+        gridPane.setPadding(new Insets(10));
+        if (!(productList.isEmpty())) {
+            int productListSize = productList.size();
+            int rowNr = 0;
+            for (int i = 0; i < productListSize - 1; i=i+4) {
+                gridPane.addRow(rowNr);
+                rowNr++;
+            }
+            int rowNrAgain = 0;
+            double magicalHeight = 483.0;
+            int magicalIdNr = 0;
+            for (int i = 0; i < productListSize - 1; i+=4) {
+                for (int j = 0; j < 4; j++) {
+                    Button newButton = new Button("" + magicalIdNr);
+                    newButton.setPrefWidth(200);
+                    newButton.setPrefHeight(250);
+                    gridPane.add(newButton, j, rowNrAgain);
+                    magicalIdNr++;
+                }
+                if (rowNrAgain < rowNr) {
+                    rowNrAgain++;
+                    apGridWindow.setPrefHeight(magicalHeight + 250);
+                    magicalHeight = magicalHeight + 250;
+                }
+            }
+        }
+        else {
+            System.out.println("Productlist is empty. ");
+        }
 
     }
 
