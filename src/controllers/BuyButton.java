6e7f3853672ef5dddeb8d8c0119ad97fa54de983
Rstrandell <razmus@student.chalmers.se>
2016-03-01 22:05:05 +0100

    köpknapp

---
+++ src/controllers/BuyButton.java
@@ -0,0 +1,18 @@
+package controllers;
+
+import javafx.scene.control.Button;
+
+/**
+ * Created by Razmus on 2016-03-01.
+ */
+public class BuyButton extends Button {
+    private int productId;
+
+    public BuyButton(String text,int id){
+        setText(text);
+        this.productId = id;
+    }
+    public int getProductId(){
+        return this.productId;
+    }
+}
