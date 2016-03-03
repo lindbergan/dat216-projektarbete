0eb032632a019fe4d3bcf556f40dfd1590d3035e
Rstrandell <razmus@student.chalmers.se>
2016-03-03 20:01:22 +0100

    semifärdig sökfunktion

---
+++ src/controllers/SearchViewController.java
@@ -0,0 +1,57 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Product;
+
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.List;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Razmus on 2016-03-03.
+ */
+public class SearchViewController implements Initializable {
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    private String input;
+    @FXML
+    GridPane gridRazz;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        List<Product> products = handler.getProducts();
+        try{
+            Properties prop = new Properties();
+            InputStreamReader in = new FileReader("search.txt");
+            prop.load(in);
+            input = prop.getProperty("input");
+        }
+        catch(Exception ex){
+            ex.getStackTrace();
+        }
+        for(int i = 0; i<products.size(); i++){
+            if (products.get(i).getName().toLowerCase().contains(input)){
+                Button newButton = new Button();
+                newButton.setPrefWidth(200);
+                newButton.setPrefHeight(250);
+                String url = "/products/images/" + products.get(i).getImageName();
+                ImageView img = new ImageView(new Image(url));
+                img.setFitWidth(newButton.getPrefWidth());
+                img.setFitHeight(newButton.getPrefHeight()*0.6);
+                img.setEffect(new DropShadow(8, Color.BEIGE));
+                newButton.setGraphic(img);
+                gridRazz.add(newButton,0,0);
+            }
+        }
+    }
+}
