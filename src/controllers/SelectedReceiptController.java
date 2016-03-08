bfd26aac7408ce27eb0f0efb6c55636657c9230b
Adrian Lindberg <adrlin@student.chalmers.se>
2016-03-08 17:05:54 +0100

    nya ändringar nya tider nya vyer. Allting fungerar utmärkt. Förutom att indexen är av någon anledning fel på kvittorna. Men det kan jag säkert fixa imorgon.

--- src/controllers/SelectedReceiptController.java
+++ src/controllers/SelectedReceiptController.java
@@ -1,7 +1,5 @@
 package controllers;
 
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,25 +10,59 @@ import javafx.scene.control.ListView;
 import javafx.scene.text.Text;
 import javafx.util.Callback;
 import properties.ListCellProducts;
-import properties.ListCellReceipts;
+import properties.ViewChanger;
+import se.chalmers.ait.dat215.project.IMatDataHandler;
+import se.chalmers.ait.dat215.project.Order;
+import se.chalmers.ait.dat215.project.ShoppingItem;
 
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class SelectedReceiptController implements Initializable {
 
     @FXML private Button backButton;
-    @FXML private Button manageButton;
-    @FXML private Button editButton;
-    @FXML private Button addButton;
     @FXML private ListView listView;
     @FXML private Text totalText;
+    IMatDataHandler handler = IMatDataHandler.getInstance();
+    public static int variable = 0;
+    static Order o;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        ViewChanger vc = new ViewChanger();
+        backButton.setOnAction(e -> {
+            try {
+                vc.changeScene(IMatController.contentProperty, "/fxml/Receipts.fxml");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
 
+        ObservableList<ShoppingItem> items = FXCollections.observableArrayList();
+        items.addAll(o.getItems().stream().collect(Collectors.toList()));
+
+        listView.setCellFactory(new Callback<ListView, ListCell>() {
+            @Override
+            public ListCell call(ListView param) {
+                return new ListCellProducts(o, items);
+            }
+        });
+        variable = 0;
+        listView.setItems(items);
+
+        double sum = 0;
+
+        for (ShoppingItem shi: o.getItems()) {
+            sum = sum + (shi.getProduct().getPrice()*shi.getAmount());
+        }
+        totalText.setText(String.valueOf(sum));
+
+    }
+
+    public static void setOrder(Order op) {
+        o = op;
     }
 }
